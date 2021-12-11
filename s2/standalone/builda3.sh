d5f50a2569f258f88c2856d253764d7503fc65ca
Ted Kirkpatrick <ted@sfu.ca>
2021-12-11 13:07:55 -0800

    Add A3 code
    Assignment 3 is based on the common s2/standalone infrastructure.  It
    adds files and updates as necessary to pull the music service down to
    the remote instance and run it.
    
    The commit adopts the convention that each of A1--A3 will have
    different source files, suffixed by their assignment (`-a3.py`).
    Assignments 1 and 2 will have this change made in future commits.
    
    This commit updates builda3.sh and Makefile to build the A3 version
    similarly to A2 but students are requested not to use these.  Instead,
    they should use docker commands directly, as specified in the
    written assignment.
    
    The commit also includes some code and infrastructure for building
    different versions of the image, including one or two extra layers with
    a file of 4KiB random bytes.  These are not currently used in the
    written assignment but retained here in case they prove useful
    later.

--- s2/standalone/builda3.sh
+++ s2/standalone/builda3.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-# Build the service with the code for A2
+# Build the service with the code for A3
+# Note: do not use this in the actual A3.
+# That assignment requests students to use the
+# actual docker calls to build the container.
 make VER=v0.75 build-s2
