7941b2e2d61ff61a7dacd83c33eb4427d2586bab
Ted Kirkpatrick <ted@sfu.ca>
2021-12-22 10:50:17 -0800

    Install docker-compose v2 in system-side CI
    Apparently (see the comments in `ci-system-v1.1.yaml`) we have to
    install docker-compose v2 if we are to guarantee its presence at
    a known path.
    
    Whatever, man.  I am so over this docker-compose version drama.
    
    This commit adds the install step to the v1.1 CI action. It
    also modifies runci.sh to accept an environment variable that
    specifies the path to the downloaded.  This variable is only
    set in GH Actions, to ensure we get the version we oh-so-carefully
    downloaded.

--- ci/runci.sh
+++ ci/runci.sh
@@ -13,6 +13,12 @@ if [[ $# -gt 1 ]]; then
   echo
   echo "  VERSION the version subdirectory of the test code (v1, ...)"
   echo "  default: v1"
+  echo
+  echo "  Environment variable COMPOSE_PATH, if defined, specifies the path used"
+  echo "  to invoke docker-compose v2.  If not defined, the path is searched"
+  echo "  for docker-compose. This variable is typically unset for local runs"
+  echo "  and only set in GitHub actions, where we need to guarantee the version"
+  echo "  of docker-compose we're using."
   exit 1
 elif [[ $# -eq 1 ]]; then
   ver="${1}"
@@ -25,12 +31,14 @@ if [[ ! -d ${ver} ]]; then
   exit 2
 fi
 
+COMP=${COMPOSE_PATH:-docker-compose}
+
 set -o xtrace
 # Turn off errexit so we continue even if CI test returns failure
 set +o errexit
-docker-compose -f ${ver}/compose.yaml up --build --abort-on-container-exit --exit-code-from test
+${COMP} -f ${ver}/compose.yaml up --build --abort-on-container-exit --exit-code-from test
 # Return code from 'up' is the test result
 trc=$?
 # Shutdown and delete all the containers before returning
-docker-compose -f ${ver}/compose.yaml down
+${COMP} -f ${ver}/compose.yaml down
 exit ${trc}
