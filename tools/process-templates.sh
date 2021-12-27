1194f7d981fbc35e9bf1dbddac818d2fbbcb9cde
Ted Kirkpatrick <ted@sfu.ca>
2021-12-27 15:40:14 -0800

    Add error checking to template processing
    The template script now checks for common configuratione errors
    and prints readable errors, rather than just failing incomprehensibly.

--- tools/process-templates.sh
+++ tools/process-templates.sh
@@ -19,13 +19,31 @@ check_command 'curl'
 # AWS command is required for all vendors, to control DynamoDB
 check_command 'aws'
 #
+# Step 0: Check that the input file exists
+#
+if [[ ! -f cluster/tpl-vars.txt ]]; then
+  echo "Create and fill in file 'cluster/tpl-vars.txt' before running this command."
+  exit 1
+fi
+#
 # Step 1: Strip comments from file listing variables
 #
 grep -v '^#' ./cluster/tpl-vars.txt > ./cluster/tpl-nocomments.txt
 #
-# Step 2: Convert the files
+# Step 2: Check that every field has a value
 #
-find . -name '*-tpl.*' -exec ./tools/call-sed.sh '{}' $(cut -f 2 -d= ./cluster/tpl-nocomments.txt) \;
+vals=$(cut -f 2 -d= ./cluster/tpl-nocomments.txt)
+count_vals () {
+  echo $#
+}
+if [[ `count_vals ${vals}` -ne 5 ]]; then
+  echo "Some fields in 'cluster/tpl-vars.txt' are not filled in."
+  exit 1
+fi
+#
+# Step 3: Convert the files
+#
+find . -name '*-tpl.*' -exec ./tools/call-sed.sh '{}'  ${vals} \;
 #
 # Step 3: Cleanup
 #
