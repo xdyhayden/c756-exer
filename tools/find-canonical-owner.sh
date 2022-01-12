8d9047cca2c2e652d3a170c00fb587865bacf556
George Chow <george@overcoil.com>
2022-01-12 10:41:15 -0800

    simplify tools/find-canonical-owner.sh

--- tools/find-canonical-owner.sh
+++ tools/find-canonical-owner.sh
@@ -10,13 +10,12 @@
 reponame=${1}
 repoowner=${2}
 
-# strip out the owning organization to reduce the required update for when the repo/assignment moves
-barename=`echo ${reponame} | cut -d \/ -f 2`
-
-if [ ${barename} = "c756-exer" ]; then
+  # the literals below need to be adjusted when:
+  #  1) the template repo (c756-exer) is renamed;
+  #  1) the template repo resides in a different organization than scp756-221); or
+  #  2) the assignment is renamed from "assignments".
+if [ ${reponame} = "scp756-221/c756-exer" ]; then
   echo "canonical-owner="${repoowner}
 else
-  # the fragment below will need to be adjusted when 1) the template repo moves to a new organization or 
-  #  2) the assignment is renamed.
   echo "caonical-owner="${reponame} | sed -e "s/scp756-221\/assignments-//" 
 fi
