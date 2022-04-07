8e64aa3bd6bc8da3f6b583a3d4b91f65f0696a55
Hayden Yan <xdy@sfu.ca>
2022-04-07 13:55:13 -0700

    s3

---
+++ s3/README.md
@@ -0,0 +1,10 @@
+# CMPT 756 Playlist service
+
+The playlist service maintains a list of playlists with songs.
+
+This is a public Flask server written in Python.
+
+There are several versions, each in its own subdirectory:
+
+v1: A version that relies upon the DB service to store its
+  values persistently.
---
+++ s3/v1/app.py
@@ -0,0 +1,151 @@
+"""
+SFU CMPT 756
+Sample application---music service.
+"""
+
+# Standard library modules
+import logging
+import sys
+
+# Installed packages
+from flask import Blueprint
+from flask import Flask
+from flask import request
+from flask import Response
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+import requests
+
+import simplejson as json
+
+
+# The application
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Playlist process')
+
+db = {
+    "name": "http://cmpt756db:30002/api/v1/datastore",
+    "endpoint": [
+        "readplaylist",
+        "writeplaylist",
+        "deletesong",
+        "playlists"
+    ]
+}
+bp = Blueprint('app', __name__)
+
+
+@bp.route('/health')
+@metrics.do_not_track()
+def health():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/readiness')
+@metrics.do_not_track()
+def readiness():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/', methods=['GET'])
+def all():
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.get(
+        url,
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/<playlist_title>', methods=['GET'])
+def read_playlist(playlist_title):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    payload = {"objtype": "playlist", "objkey": playlist_title}
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/', methods=['POST'])
+def add_song():
+    headers = request.headers
+
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        Artist = content['Artist']
+        SongTitle = content['SongTitle']
+        PlaylistTitle = content['PlaylistTitle']
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    url = db['name'] + '/' + db['endpoint'][1]
+    response = requests.post(
+        url,
+        json={"objtype": "playlist",
+              "Artist": Artist,
+              "SongTitle": SongTitle,
+              "PlaylistTitle": PlaylistTitle},
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/<playlist_title>/<music_id>', methods=['DELETE'])
+def delete_song(playlist_title, music_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    payload = {
+        "objtype": "playlist",
+        "PlaylistTitle": playlist_title,
+        "music_id": music_id
+    }
+    url = db['name'] + '/' + db['endpoint'][2]
+    response = requests.delete(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/shutdown', methods=['GET'])
+def shutdown():
+    # From https://stackoverflow.com/questions/15562446/how-to-stop-flask-application-without-using-ctrl-c # noqa: E501
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    return {}
+
+
+app.register_blueprint(bp, url_prefix='/api/v1/playlist/')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        logging.error("missing port arg 1")
+        sys.exit(-1)
+
+    p = int(sys.argv[1])
+    app.run(host='0.0.0.0', port=p, threaded=True)
